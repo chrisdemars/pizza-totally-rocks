0e3a2bef551a27653d000b9d56a1b5b37fe31dcc
Kelly J Andrews <kelly@kellyjandrews.com>
2018-09-12 21:15:13 -0400

    fixed rendering data issue

--- src/App.js
+++ src/App.js
@@ -12,28 +12,30 @@ class App extends Component {
   }
   getStorage = () => {
     let json = localStorage.getItem("recipes");
-    if (json) {
+    if (!json) {
       json = [];
       this.setStorage(json);
     }
     return json;
   }
-  getRecipe = async (e) => {
+  getRecipe = async () => {
     // const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault();
+    // e.preventDefault();
     const api_call = await fetch(`https://api.edamam.com/search?q=pizza&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`);
     const data = await api_call.json();
-    this.setState({ recipes: data.recipes });
+    this.setStorage(data.hits);
+    return data.hits;
   }
-  componentWillMount = () => {
-    let recipes = this.getStorage();
-    this.setState({ recipes });
+  componentWillMount = async () => {
+    let recipes = await this.getRecipe();
+    this.setState({ "recipes": recipes });
   }
-  componentDidUpdate = () => {
-    const recipes = JSON.stringify(this.state.recipes);
-    this.setStorage(recipes);
-  }
+  // componentDidUpdate = () => {
+  //   const recipes = JSON.stringify(this.state.recipes);
+  //   this.setStorage(recipes);
+  // }
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -9,20 +9,23 @@ const Recipes = props => (
       return (
         <div key={recipe.label} className="col-md-4" style={{ marginBottom:"2rem" }}>
           <div className="recipes__box">
-            <img 
-              className="recipe__box-img" 
-              src={recipe.image} 
+            <img
+              className="recipe__box-img"
+              src={recipe.image}
               alt={recipe.label}/>
               <div className="recipe__text">
                 <h5 className="recipes__title">
-                  { recipe.label.length < 20 ? `${recipe.label}` : `${recipe.label.substring(0, 25)}...` }
+                  { recipe.label }
+                  {
+                    // recipe.label && recipe.label.length < 20 ? `${recipe.label}` : `${recipe.label.substring(0, 25)}...`
+                  }
                 </h5>
                 <p className="recipes__subtitle">Publisher: <span>
                   { recipe.source }
                 </span></p>
               </div>
               <button className="recipe_buttons">
-                <Link to={{ 
+                <Link to={{
                   pathname: `/recipe/${recipe.uri}`,
                   state: { recipe: recipe.label }
                 }}>View Recipe</Link>
